3decfc86369fa47538726b40c88582eafcec194b
Farhan Pradana Tallei <farhan.pradana55@gmail.com>
2023-04-23 23:34:50 +0700

    cookie test

--- modules/auth/auth.handlers.js
+++ modules/auth/auth.handlers.js
@@ -29,7 +29,9 @@ const LoginHandler = (request, reply) => __awaiter(void 0, void 0, void 0, funct
     reply.setCookie('jwt_token', refreshToken, {
         signed: true,
         httpOnly: true,
-        sameSite: true,
+        secure: true,
+        domain: 'kind-pear-hatchling-yoke.cyclic.app',
+        path: '/api/auth',
     });
     const accessToken = jsonwebtoken_1.default.sign({}, env_1.ACCESS_TOKEN_SECRET, { expiresIn: '15m' });
     return { token: accessToken };
@@ -48,7 +50,9 @@ const RefreshTokenHandler = (request, reply) => __awaiter(void 0, void 0, void 0
     reply.setCookie('jwt_token', newRefreshToken, {
         signed: true,
         httpOnly: true,
-        sameSite: true,
+        secure: true,
+        domain: 'kind-pear-hatchling-yoke.cyclic.app',
+        path: '/api/auth',
     });
     const newAccessToken = jsonwebtoken_1.default.sign({}, env_1.ACCESS_TOKEN_SECRET, {
         expiresIn: '15m',
