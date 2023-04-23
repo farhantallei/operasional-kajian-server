f8d8cb5add5262bd95fcfa4e706bee8e20e7d7f7
Farhan Pradana Tallei <farhan.pradana55@gmail.com>
2023-04-23 22:41:25 +0700

    same site no

--- modules/auth/auth.handlers.js
+++ modules/auth/auth.handlers.js
@@ -29,7 +29,6 @@ const LoginHandler = (request, reply) => __awaiter(void 0, void 0, void 0, funct
     reply.setCookie('jwt_token', refreshToken, {
         signed: true,
         httpOnly: true,
-        sameSite: true,
     });
     const accessToken = jsonwebtoken_1.default.sign({}, env_1.ACCESS_TOKEN_SECRET, { expiresIn: '15m' });
     return { token: accessToken };
@@ -48,7 +47,6 @@ const RefreshTokenHandler = (request, reply) => __awaiter(void 0, void 0, void 0
     reply.setCookie('jwt_token', newRefreshToken, {
         signed: true,
         httpOnly: true,
-        sameSite: true,
     });
     const newAccessToken = jsonwebtoken_1.default.sign({}, env_1.ACCESS_TOKEN_SECRET, {
         expiresIn: '15m',
