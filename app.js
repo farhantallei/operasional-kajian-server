2b198e6216de7070ad9b7cf0aa70644917220396
Farhan Pradana Tallei <farhan.pradana55@gmail.com>
2023-04-23 17:12:18 +0700

    cors

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.addRoutes = exports.addPlugins = void 0;
 const cookie_1 = __importDefault(require("@fastify/cookie"));
+const cors_1 = __importDefault(require("@fastify/cors"));
 const sensible_1 = __importDefault(require("@fastify/sensible"));
 const fastify_1 = __importDefault(require("fastify"));
 const env_1 = require("./env");
@@ -12,6 +13,10 @@ const routes_1 = require("./routes");
 const app = (0, fastify_1.default)().withTypeProvider();
 function addPlugins() {
     app.register(cookie_1.default, { secret: env_1.COOKIE_SECRET });
+    app.register(cors_1.default, {
+        credentials: true,
+        origin: 'http://192.168.1.6:5173',
+    });
     app.register(sensible_1.default);
 }
 exports.addPlugins = addPlugins;
