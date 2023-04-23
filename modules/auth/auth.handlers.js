5a499e60e1680a4fbc9624de170a2be4fa0172a4
Farhan Pradana Tallei <farhan.pradana55@gmail.com>
2023-04-23 20:46:23 +0700

    cookies unsigned

--- modules/auth/auth.handlers.js
+++ modules/auth/auth.handlers.js
@@ -26,11 +26,7 @@ const LoginHandler = (request, reply) => __awaiter(void 0, void 0, void 0, funct
     const refreshToken = jsonwebtoken_1.default.sign({}, env_1.REFRESH_TOKEN_SECRET, {
         expiresIn: '3d',
     });
-    reply.setCookie('jwt_token', refreshToken, {
-        signed: true,
-        httpOnly: true,
-        sameSite: true,
-    });
+    reply.setCookie('jwt_token', refreshToken, {});
     const accessToken = jsonwebtoken_1.default.sign({}, env_1.ACCESS_TOKEN_SECRET, { expiresIn: '15m' });
     return { token: accessToken };
 });
@@ -39,17 +35,10 @@ const RefreshTokenHandler = (request, reply) => __awaiter(void 0, void 0, void 0
     const signedRefreshToken = request.cookies.jwt_token;
     if (!signedRefreshToken)
         return reply.unauthorized('Not authenticated');
-    const { value: refreshToken } = reply.unsignCookie(signedRefreshToken);
-    if (refreshToken == null)
-        return reply.forbidden('Token is invalid');
     const newRefreshToken = jsonwebtoken_1.default.sign({}, env_1.REFRESH_TOKEN_SECRET, {
         expiresIn: '3d',
     });
-    reply.setCookie('jwt_token', newRefreshToken, {
-        signed: true,
-        httpOnly: true,
-        sameSite: true,
-    });
+    reply.setCookie('jwt_token', newRefreshToken, {});
     const newAccessToken = jsonwebtoken_1.default.sign({}, env_1.ACCESS_TOKEN_SECRET, {
         expiresIn: '15m',
     });
